5495fd99d076e18dad3f0d39fb4e442739b0c753
CrystalisMirage <k1914490@students.katyisd.org>
2021-06-03 16:38:56 -0500

    Added Info command

---
+++ commands/info.js
@@ -0,0 +1,34 @@
+const { MessageEmbed }=require('discord.js')
+module.exports.help={
+    name:'info',
+    aliases:['stats','whois']
+}
+
+module.exports.run=async(client, message, args)=>{
+    let person=message.mentions.members.first()||message.guild.members.cache.get(args[1])||message.member
+    let embed=new MessageEmbed()
+    .setTitle(`${person.displayName}'s Info`)
+    .setAuthor(message.guild.members.cache.get(client.user.id).displayName,client.user.displayAvatarURL({dynamic:true}))
+    .setThumbnail(person.displayAvatarURL({dynamic:true}))
+    .setDescription(`Some info about ${person}`)
+    .addFields(
+        {
+            name:'Display Name',
+            value:person.displayName,
+            inline:true
+        },
+        {
+            name:'Username',
+            value:person.user.tag,
+            inline:true
+        },
+        {
+            name:'Is A bot?',
+            value:person.user.bot
+        }
+    )
+    .setColor('ORANGE')
+    .setTimestamp()
+    .setFooter(`${client.user.username}||${message.guild.name}`,client.user.displayAvatarURL({dynamic:true}))
+    message.channel.send(embed)
+}
